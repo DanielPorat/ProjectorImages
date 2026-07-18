04416cf4d85bb3d0609e28b1e3b3bd5e2adb4675
DanielPorat <gamillion0@gmail.com>
2026-07-18 00:29:02 -0400

    Adding basic Button pressing functionality

--- main.py
+++ main.py
@@ -2,15 +2,27 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import tkinter as tk
+from PIL import Image
+Lake = 'Res/BackGroundLake.jpg'
+League = 'Res/Aatrox.jpg'
+def PopUpImage(ImagePath):
+    img = Image.open(ImagePath)
+    img.show()
+def OnKeyPress(event):
+    if event.char == 'W':
+        PopUpImage(Lake)
+        print("Switching to Cloud!")
+    elif event.char == 'P':
+        PopUpImage(League)
+    else:
+        print("Unrecongized")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    root = tk.Tk()
+    root.bind("<Key>", OnKeyPress)
+    root.mainloop()
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
